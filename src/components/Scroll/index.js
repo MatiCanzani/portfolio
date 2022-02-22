b2baaea042166fcf5936c6d2d33dcd9460593344
Mati-Bowery <matias@bowerystudio.co>
2022-02-22 22:03:52 +0000

    add  keyboard

--- src/components/Scroll/index.js
+++ src/components/Scroll/index.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 
 import "./Scroll.scss";
 
+const isBrowser = () => typeof window !== "undefined";
+
 const Scroll = () => {
   const [scroll, setScroll] = useState(true);
 
   const handleScroll = () => {
-    window.scrollY <= 80 ? setScroll(true) : setScroll(false);
+    if (isBrowser) {
+      window.addEventListener("scroll", handleScroll);
+      window.scrollY <= 80 ? setScroll(true) : setScroll(false);
+    } 
   };
-  window.addEventListener("scroll", handleScroll);
   return (
     <>
       <div className={scroll ? "scroll" : "scroll__hidden"}>
